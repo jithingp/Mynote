6fa8330a3dedb48d11739c33fdb18489663ca616
jithingp <jithingpanoli18@gmail.com>
2021-12-27 13:20:44 +0530

    login endpoint created

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,6 +7,7 @@ var jwt = require('jsonwebtoken')
 
 const JWT_SECRET = 'vinfraubrot$casa'
 
+//signup
 router.post('/createuser',[
     body('name','Enter a valid name').isLength({ min: 3 }),
     body('email','Enter a valid email').isEmail(),
@@ -40,10 +41,46 @@ router.post('/createuser',[
 
     }catch(error){
         console.log(error.message);
-        res.status(500).send("Some Error ocurred")
+        res.status(500).send("Internal Server Error")
     }
 
-}) 
+})
+//login
+router.post('/login',[
+    body('email','Enter a valid email').isEmail(),
+    body('password','password cannot be blank').exists(),
+],async(req,res)=>{ 
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {email,password} = req.body
+    try {
+        let user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({error:"Please enter correct credentials"})
+         }
+         const passwordCompare = await bcrypt.compare(password,user.password)
+         if(!passwordCompare){
+            return res.status(400).json({error:"Please enter correct credentials"})
+         }
+         const data = {
+            user:{
+                id: user.id
+            }
+        }
+        const authtoken= jwt.sign(data,JWT_SECRET)
+        
+        res.json({authtoken})
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Internal Server Error")
+    }
+
+    
+
+})   
 
 
 module.exports=router
