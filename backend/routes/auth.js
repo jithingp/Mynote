944a3ef0b2918b521464972042f1cc4cf3c167b5
jithingp <jithingpanoli18@gmail.com>
2021-12-27 12:07:19 +0530

    bcryptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,8 +2,10 @@ const express=require('express')
 const router = express.Router()
 const User=require('../models/User')
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs')
+var jwt = require('jsonwebtoken')
 
-
+const JWT_SECRET = 'vinfraubrot$casa'
 
 router.post('/createuser',[
     body('name','Enter a valid name').isLength({ min: 3 }),
@@ -20,12 +22,21 @@ router.post('/createuser',[
         if(user){
             return res.status(400).json({error:"Sorry a user with this email already exists"})
         }
+        const salt = await bcrypt.genSalt(10)
+        const secPass = await bcrypt.hash(req.body.password,salt)
         user=await  User.create({
             name: req.body.name,
-            password: req.body.password,
+            password: secPass,
             email: req.body.email
           })
-          res.json(user)
+          const data = {
+              user:{
+                  id: user.id
+              }
+          }
+          const authtoken= jwt.sign(data,JWT_SECRET)
+          
+          res.json({authtoken})
 
     }catch(error){
         console.log(error.message);
