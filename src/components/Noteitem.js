95d7cb8446987eb89ad207d7d81ee10032ac72f1
jithingp <jithingpanoli18@gmail.com>
2021-12-31 18:10:36 +0530

    font awesome added

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -4,14 +4,15 @@ const Noteitem = (props) => {
   const { note } = props;
   return (
     <div className="col-md-3">
+      <div className="card my-3">
+        <div className="card-body">
+          <div className="d-flex align-items-center">
+            <h5 className="card-title">{note.title}</h5>
+            <i className="fas fa-trash-alt mx-2"></i>
+            <i className="fas fa-edit mx-2"></i>
+          </div>
 
-      <div class="card my-3" >
-        <div class="card-body">
-          <h5 class="card-title">{note.title}</h5>
-          <p class="card-text">
-           {note.description}
-          </p>
-         
+          <p className="card-text">{note.description}</p>
         </div>
       </div>
     </div>
